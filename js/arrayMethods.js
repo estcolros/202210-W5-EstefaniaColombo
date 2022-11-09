ce9c7cd2a68c4efa8792eb967ef6c7281b014770
estefaniacr <estefania.c.r@hotmail.com>
2022-11-09 20:01:06 +0100

    Adding array methods (push, pop and indexOf) and tests

--- js/arrayMethods.js
+++ js/arrayMethods.js
@@ -1,7 +1,26 @@
 export const aLength = (array) => {
     let cont = 0;
-    for (let i = 0; array[i] !== undefined || null; i++) {
+    for (let i = 0; array[i] !== undefined; i++) {
         cont++;
     }
     return cont;
+};
+
+export const aPush = (array, item) => {
+    array[aLength(array)] = item;
+    return aLength(array);
+};
+
+export const aPop = (array) => {
+    let lastElem = array[aLength(array) - 1];
+    array.aLength = array.aLength - 1;
+    return lastElem;
+};
+
+export const aIndexOf = (array, item) => {
+    for (let i = 0; i < aLength(array); i++) {
+        if (array[i] === item) {
+            return i;
+        }
+    }
 };
--- js/arrayMethods.test.js
+++ js/arrayMethods.test.js
@@ -1,11 +1,38 @@
-import { aLength } from "./arrayMethods";
+import { aLength, aPush, aPop, aIndexOf } from "./arrayMethods";
 
-describe("Given length function", () => {
-    describe("When it is run estefania", () => {
-        test("Then it should return estefania.length", () => {
-            const name = [8];
-            const result = aLength(name);
-            expect(result).toBe(name.length);
-        });
+describe("Given array length function", () => {
+    const testLength = ["e", "s", "t", "e", "f", "a", "n", "i", "a", null, NaN];
+    test(`When it is run ${testLength}, the length of the array should be ${testLength.length}`, () => {
+        const result = aLength(testLength);
+        const expectResult = testLength.length;
+        expect(result).toBe(expectResult);
+    });
+});
+
+describe("Given array push function", () => {
+    const testPush = [1, 2, 3];
+    const expectResult = [1, 2, 3, 13];
+    test(`Given array ${testPush}, expected length of ${aLength(
+        expectResult
+    )}`, () => {
+        aPush(testPush, 13);
+        expect(testPush).toStrictEqual(expectResult);
+    });
+});
+
+describe("Given array pop function", () => {
+    const testPop = [1, 2, 3, "four"];
+    const expectResult = "four";
+    test(`When the pop method is executed, the last element ${testPop} of the array is removed ${expectResult}`, () => {
+        expect(aPop(testPop)).toBe(expectResult);
+    });
+});
+
+describe("Given array indexOf function", () => {
+    const testIndexOf = [1, 2, 3];
+    const expectResult = 2;
+    test(`When the array parameter is ${testIndexOf[2]}, the index result has the value of ${expectResult}`, () => {
+        const result = aIndexOf(testIndexOf, 3);
+        expect(result).toBe(expectResult);
     });
 });
